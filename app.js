db67928c2a1d4c46285a55b8cc9e4996fd7ce07e
Kush12203 <130508276+Kush12203@users.noreply.github.com>
2023-09-02 22:45:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -51,9 +51,11 @@ app.use(mongoSanitize({
     replaceWith:'_'
 }));
 
+const secret = process.env.SECRET ;
+
 const store=new MongoDBStore({
     url:dbUrl,
-    secret:'thisshouldbeabettersecret!',
+    secret,
     touchAfter:24*60*60
 });
 
@@ -64,7 +66,7 @@ store.on("error",function(e){
 const sessionConfig = {
     store,
     name:'session',
-    secret:'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
